eec676199b671a53ed6b2e7424d4a9abdd94e13b
sami8890 <samigabol12@gmail.com>
2025-04-30 10:15:51 +0500

    added folders

--- practive-set.py
+++
@@ -1,9 +0,0 @@
-# fruits = ["apple", "banana", "cherry"]
-# fruits[2] = "grapes"  
-# print(fruits)
-
-
-x = "js"
-  # This will raise an error because integers do not have an append method
-print(x)
-
